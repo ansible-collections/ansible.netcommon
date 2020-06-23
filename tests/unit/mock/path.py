fb61a292ee4caaf9f4f0dc97edaae1e06dcf9ac4
Nathaniel Case <Qalthos@gmail.com>
2020-06-23 17:40:39 -0400

    Clean up bolerplate ignores

--- tests/unit/mock/path.py
+++ tests/unit/mock/path.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
 from ansible_collections.ansible.netcommon.tests.unit.compat.mock import (
     MagicMock,
 )
