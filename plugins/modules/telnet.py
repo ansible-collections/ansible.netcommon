0b6271839f9ea9c13d86e545db751bc7248f4f89
Nathaniel Case <Qalthos@gmail.com>
2020-06-24 10:55:03 -0400

    Add types to telnet arguments

--- plugins/modules/telnet.py
+++ plugins/modules/telnet.py
@@ -20,56 +20,68 @@ options:
     description:
     - List of commands to be executed in the telnet session.
     required: true
+    type: list
+    elements: str
     aliases:
     - commands
   host:
     description:
     - The host/target on which to execute the command
     required: false
+    type: str
     default: remote_addr
   user:
     description:
     - The user for login
     required: false
+    type: str
     default: remote_user
   password:
     description:
     - The password for login
+    type: str
   port:
     description:
     - Remote port to use
+    type: int
     default: 23
   timeout:
     description:
     - timeout for remote operations
+    type: int
     default: 120
   prompts:
     description:
     - List of prompts expected before sending next command
     required: false
+    type: list
+    elements: str
     default:
     - $
   login_prompt:
     description:
     - Login or username prompt to expect
     required: false
+    type: str
     default: 'login: '
   password_prompt:
     description:
     - Login or username prompt to expect
     required: false
+    type: str
     default: 'Password: '
   pause:
     description:
     - Seconds to pause between each command issued
     required: false
+    type: int
     default: 1
   send_newline:
     description:
     - Sends a newline character upon successful connection to start the terminal session.
     required: false
-    default: false
     type: bool
+    default: false
 notes:
 - The C(environment) keyword does not work with this task
 author:
