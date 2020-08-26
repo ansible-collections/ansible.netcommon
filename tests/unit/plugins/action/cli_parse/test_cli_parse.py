793cd754cceb5812f34f0f5788935d2f9d45914d
Bradley A. Thornton <bthornto@thethorntons.net>
2020-08-26 07:12:40 -0700

    Additional test for cli_parse

--- tests/unit/plugins/action/cli_parse/test_cli_parse.py
+++ tests/unit/plugins/action/cli_parse/test_cli_parse.py
@@ -5,12 +5,14 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-
+import tempfile
 from ansible.playbook.task import Task
 from ansible.template import Templar
+
 from ansible_collections.ansible.netcommon.tests.unit.compat import unittest
 from ansible_collections.ansible.netcommon.tests.unit.compat.mock import (
     MagicMock,
+    patch,
 )
 from ansible_collections.ansible.netcommon.tests.unit.mock.loader import (
     DictDataLoader,
@@ -199,6 +201,9 @@ class TestCli_Parse(unittest.TestCase):
             self.assertEqual(result, check[2])
 
     def test_fn_update_template_path_not_exist(self):
+        """ Check the creation of the template_path if
+        it doesn't exist in the user provided data
+        """
         self._plugin._task.args = {
             "parser": {"command": "a command", "name": "a.b.c"}
         }
@@ -209,3 +214,71 @@ class TestCli_Parse(unittest.TestCase):
             "Could not find or access 'nxos_a_command.yaml'",
             str(error.exception),
         )
+
+    def test_fn_get_template_contents_pass(self):
+        """ Check the retrieval of the template contents
+        """
+        temp = tempfile.NamedTemporaryFile()
+        contents = "abcdef"
+        with open(temp.name, "w") as fileh:
+            fileh.write(contents)
+
+        self._plugin._task.args = {"parser": {"template_path": temp.name}}
+        result = self._plugin._get_template_contents()
+        self.assertEqual(result, contents)
+
+    def test_fn_get_template_contents_missing(self):
+        """ Check the retrieval of the template contents
+        """
+        self._plugin._task.args = {"parser": {"template_path": "non-exist"}}
+        with self.assertRaises(Exception) as error:
+            self._plugin._get_template_contents()
+        self.assertIn(
+            "Failed to open template 'non-exist'", str(error.exception)
+        )
+
+    def test_fn_prune_result_pass(self):
+        """ Test the removal of stdout and stdout_lines from the _result
+        """
+        self._plugin._result["stdout"] = "abc"
+        self._plugin._result["stdout_lines"] = "abc"
+        self._plugin._prune_result()
+        self.assertNotIn("stdout", self._plugin._result)
+        self.assertNotIn("stdout_lines", self._plugin._result)
+
+    def test_fn_prune_result_not_exist(self):
+        """ Test the removal of stdout and stdout_lines from the _result
+        """
+        self._plugin._prune_result()
+        self.assertNotIn("stdout", self._plugin._result)
+        self.assertNotIn("stdout_lines", self._plugin._result)
+
+    def test_fn_run_command_lx(self):
+        """ Check run command for non network
+        """
+        response = "abc"
+        self._plugin._connection.socket_path = None
+        self._plugin._low_level_execute_command = MagicMock()
+        self._plugin._low_level_execute_command.return_value = {
+            "rc": 0,
+            "stdout": response,
+            "stdout_lines": response,
+        }
+        self._plugin._task.args = {"command": "ls"}
+        res = self._plugin._run_command()
+        self.assertEqual(self._plugin._result["stdout"], response)
+        self.assertEqual(self._plugin._result["stdout_lines"], response)
+
+    @patch("ansible.module_utils.connection.Connection.__rpc__")
+    def test_fn_run_command_network(self, mock_send):
+        """ Check run command for network
+        """
+        expected = "abc"
+        mock_send.return_value = expected
+        self._plugin._connection.socket_path = (
+            tempfile.NamedTemporaryFile().name
+        )
+        self._plugin._task.args = {"command": "command"}
+        res = self._plugin._run_command()
+        self.assertEqual(self._plugin._result["stdout"], expected)
+        self.assertEqual(self._plugin._result["stdout_lines"], [expected])
