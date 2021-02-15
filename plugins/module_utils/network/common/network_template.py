335d4c5ad08f779df3ed0a7948ad6837f3b10917
GomathiselviS <gomathiselvi@gmail.com>
2021-02-15 12:27:10 -0500

    modified tox ignores

--- plugins/module_utils/network/common/network_template.py
+++ plugins/module_utils/network/common/network_template.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.network_template import (
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.network_template import (  # noqa: F401
     NetworkTemplate,
 )
--- plugins/module_utils/network/common/resource_module.py
+++ plugins/module_utils/network/common/resource_module.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.resource_module import (
+from ansible_collections.ansible.netcommon.plugins.module_utils.network.common.rm_base.resource_module import (  # noqa: F401
     ResourceModule,
 )
