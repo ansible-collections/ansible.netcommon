568efd5d43e19e8ca02254dd453d5a743edb5c72
Nathaniel Case <this.is@nathanielca.se>
2021-08-09 14:26:27 -0400

    Drop use of copy module from network backup file writing.

--- plugins/action/network.py
+++ plugins/action/network.py
@@ -25,10 +25,11 @@ import time
 import re
 
 from ansible.errors import AnsibleError
-from ansible.module_utils._text import to_text, to_bytes
+from ansible.module_utils._text import to_text
 from ansible.module_utils.six.moves.urllib.parse import urlsplit
 from ansible.plugins.action.normal import ActionModule as _ActionModule
 from ansible.utils.display import Display
+from ansible.utils.hashing import checksum, checksum_s
 from ansible.module_utils.six import PY3
 
 display = Display()
@@ -118,70 +119,35 @@ class ActionModule(_ActionModule):
             filename = backup_options.get("filename")
             backup_path = backup_options.get("dir_path")
 
+        tstamp = time.strftime(
+            "%Y-%m-%d@%H:%M:%S", time.localtime(time.time())
+        )
         if not backup_path:
             cwd = self._get_working_path()
             backup_path = os.path.join(cwd, "backup")
         if not filename:
-            tstamp = time.strftime(
-                "%Y-%m-%d@%H:%M:%S", time.localtime(time.time())
-            )
             filename = "%s_config.%s" % (
                 task_vars["inventory_hostname"],
                 tstamp,
             )
 
         dest = os.path.join(backup_path, filename)
-        backup_path = os.path.expanduser(
-            os.path.expandvars(
-                to_bytes(backup_path, errors="surrogate_or_strict")
-            )
-        )
-
         if not os.path.exists(backup_path):
             os.makedirs(backup_path)
 
-        new_task = self._task.copy()
-        for item in self._task.args:
-            if not item.startswith("_"):
-                new_task.args.pop(item, None)
-
-        new_task.args.update(dict(content=content, dest=dest))
-        copy_action = self._shared_loader_obj.action_loader.get(
-            "copy",
-            task=new_task,
-            connection=self._connection,
-            play_context=self._play_context,
-            loader=self._loader,
-            templar=self._templar,
-            shared_loader_obj=self._shared_loader_obj,
-        )
-        copy_result = copy_action.run(task_vars=task_vars)
-        if copy_result.get("failed"):
-            result["failed"] = copy_result["failed"]
-            result["msg"] = copy_result.get("msg")
-            return
+        changed = False
+        if not os.path.exists(dest) or checksum(dest) != checksum_s(content):
+            with open(dest, "w") as output_file:
+                output_file.write(content)
+            changed = True
 
         result["backup_path"] = dest
-        if copy_result.get("changed", False):
-            result["changed"] = copy_result["changed"]
+        result["changed"] = changed
 
-        if backup_options and backup_options.get("filename"):
-            result["date"] = time.strftime(
-                "%Y-%m-%d",
-                time.gmtime(os.stat(result["backup_path"]).st_ctime),
-            )
-            result["time"] = time.strftime(
-                "%H:%M:%S",
-                time.gmtime(os.stat(result["backup_path"]).st_ctime),
-            )
-
-        else:
-            result["date"] = tstamp.split("@")[0]
-            result["time"] = tstamp.split("@")[1]
-            result["shortname"] = result["backup_path"][::-1].split(".", 1)[1][
-                ::-1
-            ]
-            result["filename"] = result["backup_path"].split("/")[-1]
+        result["date"], result["time"] = tstamp.split("@")
+        if not (backup_options and backup_options.get("filename")):
+            result["filename"] = os.path.basename(result["backup_path"])
+            result["shortname"] = os.path.splitext(result["backup_path"])[0]
 
         # strip out any keys that have two leading and two trailing
         # underscore characters
