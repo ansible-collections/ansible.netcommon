bad85a24c9a86b60d7577685c86b32b5b86cbfb4
cidrblock <bthornto@redhat.com>
2022-04-26 11:55:21 -0700

    test functionality

--- plugins/connection/network_cli.py
+++ plugins/connection/network_cli.py
@@ -273,6 +273,38 @@ options:
     - name: ANSIBLE_NETWORK_SINGLE_USER_MODE
     vars:
     - name: ansible_network_single_user_mode
+  test_parameters:
+    default: {}
+    type: dict
+    description:
+    - This option allows to pass additional parameters for testing purposes.
+    suboptions:
+      fixture_directory:
+        description:
+        - The directory where the test fixture files will be/are located.
+        required: true
+        type: string
+      mode:
+        choices:
+        - compare
+        - playback
+        - record
+        description:
+        - The mode in which the test fixture files will be used.
+        - I(compare): Compare the fixture to live output from the device.
+        - I(playback): Use the fixtures rather than directly interacting with the device.
+        - I(record): Record the output from the device to the fixture files.
+        required: true
+        type: string
+      exempted:
+        default: []
+        description: A list of regular expressions that will be used to exclude the output from the compare.
+        type: list
+        elements: string
+    env:
+    - name: ANSIBLE_NETWORK_TEST_PARAMETERS
+    vars:
+    - name: ansible_network_test_parameters
 """
 
 from functools import wraps
@@ -364,6 +396,9 @@ class Connection(NetworkConnectionBase):
         self._ssh_type = None
 
         self._single_user_mode = False
+
+        # Track the send sequence for testing, increment before send
+        self._send_sequence = -1
 
         if self._network_os:
             self._terminal = terminal_loader.get(self._network_os, self)
@@ -587,6 +622,9 @@ class Connection(NetworkConnectionBase):
         """
         Connects to the remote device and starts the terminal
         """
+        if self.get_option("test_parameters").get("mode") == "playback":
+            return
+
         if self._play_context.verbosity > 3:
             logging.getLogger(self.ssh_type).setLevel(logging.DEBUG)
 
@@ -995,6 +1033,10 @@ class Connection(NetworkConnectionBase):
         """
         Sends the command to the device in the opened shell
         """
+        # Check for testing
+        if self.get_option("test_parameters").get("test_mode") == "playback":
+            return self._send_playback(command)
+
         # try cache first
         if (not prompt) and (self._single_user_mode):
             out = self.get_cache().lookup(command)
@@ -1002,7 +1044,7 @@ class Connection(NetworkConnectionBase):
                 self.queue_message(
                     "vvvv", "cache hit for command: %s" % command
                 )
-                return out
+                return self._send_post(command=command, response=out)
 
         if check_all:
             prompt_len = len(to_list(prompt))
@@ -1045,7 +1087,8 @@ class Connection(NetworkConnectionBase):
                     )
                     self.get_cache().populate(command, response)
 
-            return response
+            return self._send_post(command=command, response=response)
+
         except (socket.timeout, AttributeError):
             self.queue_message("error", traceback.format_exc())
             raise AnsibleConnectionFailure(
@@ -1352,3 +1395,88 @@ class Connection(NetworkConnectionBase):
         if (self._is_in_config_mode()) or (to_text(command) in cfg_cmds):
             invalidate = True
         return invalidate
+
+    def _send_playback(self, command):
+        """Send the fixture response rather than the actual command."""
+
+        test_parameters = self.get_option("test_parameters")
+
+        self._send_sequence += 1
+
+        fixture_file = os.path.join(
+            test_parameters["fixture_directory"],
+            "%s.json" % self._send_sequence,
+        )
+        if not os.path.exists(fixture_file):
+            raise AnsibleError(
+                "Fixture file %s does not exist." % fixture_file
+            )
+
+        with open(fixture_file, "r") as f:
+            fixture = json.load(f)
+
+        if fixture["command"] != command:
+            raise AssertionError(
+                "Fixture command %s does not match command %s."
+                % (fixture["command"], command)
+            )
+        return fixture["response"]
+
+    def _send_post(self, command, response):
+        """Proxy the response to the send() method"""
+        test_parameters = self.get_option("test_parameters")
+        if not test_parameters:
+            return response
+
+        self._send_sequence += 1
+
+        fixture_file = os.path.join(
+            test_parameters["fixture_directory"],
+            "%s.json" % self._send_sequence,
+        )
+
+        if test_parameters["mode"] == "record":
+            os.makedirs(test_parameters["fixture_directory"], exist_ok=True)
+
+            with open(fixture_file, "w") as f:
+                json.dump(
+                    {"command": command.decode("utf-8"), "response": response},
+                    f,
+                )
+                f.write("\n")
+        elif test_parameters["mode"] == "compare":
+            with open(fixture_file, "r") as f:
+                fixture = json.load(f)
+            if fixture["command"] != command.decode("utf-8"):
+                raise AssertionError(
+                    (
+                        "Command sent to device does not match the recorded command",
+                        fixture["command"],
+                        command.decode("utf-8"),
+                    ),
+                )
+            fixture_lines = fixture["response"].splitlines()
+            response_lines = response.splitlines()
+            if len(fixture_lines) != len(response_lines):
+                raise AssertionError(
+                    (
+                        "Response length does not match the recorded response",
+                        len(fixture["response"]),
+                        len(response),
+                    ),
+                )
+            for idx, line in enumerate(fixture_lines):
+                if line != response_lines[idx]:
+                    if any(
+                        re.match(re_exempt, line)
+                        for re_exempt in test_parameters["exempted"]
+                    ):
+                        continue
+                    raise AssertionError(
+                        (
+                            "Response line does not match the recorded response line",
+                            line,
+                            response[idx],
+                        ),
+                    )
+        return response
