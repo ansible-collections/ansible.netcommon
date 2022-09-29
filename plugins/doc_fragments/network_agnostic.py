2aa0f39bddc3996cee3f4ea37672b8e37b421c9f
Kate Case <this.is@katherineca.se>
2022-09-29 14:45:34 -0400

    Move LICENSE, update files referring to COPYING

--- plugins/doc_fragments/network_agnostic.py
+++ plugins/doc_fragments/network_agnostic.py
@@ -4,7 +4,8 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 # Copyright: (c) 2019 Ansible, Inc
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 
 class ModuleDocFragment(object):
