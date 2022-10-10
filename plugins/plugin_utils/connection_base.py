bb4016018f01734ae36079d151a6e884e44d885c
Kate Case <kcase@redhat.com>
2022-10-10 14:44:03 -0400

    Add BSD license text, SPDX License Identifiers (#471)
    * Move LICENSE, update files referring to COPYING
    
    * Update long-form licenses
    
    * Add license to files missing them, delete empty __init__.py files
    
    * Update BSD files

--- plugins/plugin_utils/connection_base.py
+++ plugins/plugin_utils/connection_base.py
@@ -2,6 +2,9 @@
 # (c) 2015 Toshio Kuratomi <tkuratomi@ansible.com>
 # (c) 2017, Peter Sprygada <psprygad@redhat.com>
 # (c) 2017 Ansible Project
+# GNU General Public License v3.0+ (see LICENSES/GPL-3.0-or-later.txt or https://www.gnu.org/licenses/gpl-3.0.txt)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
