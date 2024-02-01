fd615d5e52ce684aea91ff792142d24f4b1d694f
KB-perByte <sagpaul@redhat.com>
2024-02-01 15:43:39 +0530

    make it minimal

--- plugins/modules/cli_restore.py
+++ plugins/modules/cli_restore.py
@@ -22,52 +22,27 @@ version_added: 6.1.0
 extends_documentation_fragment:
 - ansible.netcommon.network_agnostic
 options:
-  config:
+  filename:
     description:
-    - The config to be pushed to the network device. This argument is mutually exclusive
-      with C(rollback) and either one of the option should be given as input. To ensure
-      idempotency and correct diff the configuration lines should be similar to how they
-      appear if present in the running configuration on device including the indentation.
+    - Filename of the backup file to be restored.
     type: str
-  commit:
+  force:
     description:
-    - The C(commit) argument instructs the module to push the configuration to the
-      device. This is mapped to module check mode.
+    - The force keyword replaces the current running configuration file with the specified saved
+      configuration file without prompting you for confirmation.
     type: bool
-  rollback:
-    description:
-    - The C(rollback) argument instructs the module to rollback the current configuration
-      to the identifier specified in the argument.  If the specified rollback identifier
-      does not exist on the remote device, the module will fail. To rollback to the
-      most recent commit, set the C(rollback) argument to 0. This option is mutually
-      exclusive with C(config).
-    type: int
-  commit_comment:
-    description:
-    - The C(commit_comment) argument specifies a text string to be used when committing
-      the configuration. If the C(commit) argument is set to False, this argument
-      is silently ignored. This argument is only valid for the platforms that support
-      commit operation with comment.
-    type: str
 """
 
 EXAMPLES = """
-- name: Restore nxos configuration
+- name: Restore IOS-XE configuration
   ansible.netcommon.cli_restore:
-    config: "{{ lookup('file', 'backup_nxos.cfg') }}"
+    filename: test.cfg
+    force: false
 """
 
 RETURN = """
-backup_path:
-  description: The full path to the backup file
-  returned: always
-  type: str
-  sample: /playbooks/ansible/backup/hostname_config.2016-07-16@22:28:34
 """
 
-import json
-
-from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.connection import Connection
 
@@ -75,159 +50,40 @@ from ansible.module_utils.connection import Connection
 def validate_args(module, device_operations):
     """validate param if it is supported on the platform"""
     feature_list = []
-    import q
 
     for feature in feature_list:
         if module.params[feature]:
-            q(feature)
-            supports_feature = device_operations.get("supports_%s" % feature)
-            q(supports_feature)
+            supports_feature = device_operations.get(f"supports_{feature}")
             if supports_feature is None:
                 module.fail_json(
-                    msg="Thisdfvdfdsd platform does not specify whether %s is supported or not. "
-                    "Please report an issue against this platform's cliconf plugin." % feature
+                    msg=f"This platform does not specify whether {feature} is supported or not. "
+                    "Please report an issue against this platform's cliconf plugin."
                 )
             elif not supports_feature:
-                module.fail_json(msg="Option %s is not supported on this platform" % feature)
-
-
-def run(module, device_operations, connection, candidate, running, rollback_id):
-    result = {}
-    resp = {}
-    config_diff = []
-    banner_diff = {}
-
-    commit_comment = module.params["commit_comment"]
-
-    commit = not module.check_mode
-
-    if rollback_id is not None:
-        resp = connection.rollback(rollback_id, commit)
-        if "diff" in resp:
-            result["changed"] = True
-
-    elif device_operations.get("supports_onbox_diff"):
-        if candidate and not isinstance(candidate, list):
-            candidate = candidate.strip("\n").splitlines()
-
-        kwargs = {
-            "candidate": candidate,
-            "commit": commit,
-            "replace": None,
-            "comment": commit_comment,
-        }
-        resp = connection.edit_config(**kwargs)
-
-        if "diff" in resp:
-            result["changed"] = True
-            result["diff"] = resp["diff"]
-
-    elif device_operations.get("supports_generate_diff"):
-        kwargs = {"candidate": candidate, "running": running}
-
-        diff_response = connection.get_diff(**kwargs)
-
-        config_diff = diff_response.get("config_diff")
-        banner_diff = diff_response.get("banner_diff")
-
-        if config_diff:
-            if isinstance(config_diff, list):
-                candidate = config_diff
-            else:
-                candidate = config_diff.splitlines()
-
-            kwargs = {
-                "candidate": candidate,
-                "commit": commit,
-                "replace": None,
-                "comment": commit_comment,
-            }
-            if commit:
-                connection.edit_config(**kwargs)
-            result["changed"] = True
-            result["commands"] = config_diff.split("\n")
-
-        if banner_diff:
-            candidate = json.dumps(banner_diff)
-
-            kwargs = {"candidate": candidate, "commit": commit}
-            if commit:
-                connection.edit_banner(**kwargs)
-            result["changed"] = True
-
-    if module._diff:
-        if "diff" in resp:
-            result["diff"] = {"prepared": resp["diff"]}
-        else:
-            diff = ""
-            if config_diff:
-                if isinstance(config_diff, list):
-                    diff += "\n".join(config_diff)
-                else:
-                    diff += config_diff
-            if banner_diff:
-                diff += json.dumps(banner_diff)
-            result["diff"] = {"prepared": diff}
-
-    if result.get("changed"):
-        msg = (
-            "To ensure idempotency and correct diff the input configuration lines should be"
-            " similar to how they appear if present in"
-            " the running configuration on device including the indentation"
-        )
-        if "warnings" in result:
-            result["warnings"].append(msg)
-        else:
-            result["warnings"] = msg
-    return result
+                module.fail_json(
+                    msg=f"Option {feature} is not supported on this platform"
+                )
 
 
 def main():
     """main entry point for execution"""
     argument_spec = dict(
-        config=dict(type="str"),
-        commit=dict(type="bool"),
-        commit_comment=dict(type="str"),
+        force=dict(default=False, type="bool"),
+        filename=dict(),
     )
 
     module = AnsibleModule(
         argument_spec=argument_spec,
-        required_one_of=[
-            "config",
-        ],
-        supports_check_mode=False,
     )
 
     result = {"changed": False}
 
     connection = Connection(module._socket_path)
-    capabilities = module.from_json(connection.get_capabilities())
-    import q
 
-    q(capabilities)
-    if capabilities:
-        device_operations = capabilities.get("device_operations", dict())
-        validate_args(module, device_operations)
-    else:
-        device_operations = dict()
-
-    candidate = module.params["config"]
-    candidate = to_text(candidate, errors="surrogate_then_replace") if candidate else None
-
-    if candidate:
-        try:
-            result.update(
-                run(
-                    module,
-                    device_operations,
-                    connection,
-                    candidate,
-                    None,
-                    None,
-                )
-            )
-        except Exception as exc:
-            module.fail_json(msg=to_text(exc))
+    running = connection.restore(
+        force=module.params["force"], filename=module.params["filename"]
+    )
+    result["__restore__"] = running
 
     module.exit_json(**result)
 
