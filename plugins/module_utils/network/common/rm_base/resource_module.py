fa70d5aaa668dbb033552db52320add6dc08672e
NilashishC <nilashishchakraborty8@gmail.com>
2024-05-27 14:28:50 +0530

    Revert RMBase changes
    Signed-off-by: NilashishC <nilashishchakraborty8@gmail.com>

--- plugins/module_utils/network/common/rm_base/resource_module.py
+++ plugins/module_utils/network/common/rm_base/resource_module.py
@@ -145,9 +145,9 @@ class ResourceModule(RmEngineBase):  # pylint: disable=R0902
                 else:
                     self.addcmd(have, parser, True)
 
-    def run_commands(self, err_responses=None):
+    def run_commands(self):
         """Send commands to the device"""
         if self.commands and self.state in self.ACTION_STATES:
             if not self._module.check_mode:
-                self._connection.edit_config(candidate=self.commands, err_responses=err_responses)
+                self._connection.edit_config(candidate=self.commands)
             self.changed = True
