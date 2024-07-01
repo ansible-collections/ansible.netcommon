507e1689d83b57d08ff55072b7203c6027f1484c
rohitthakur2590 <rohitthakur2590@outlook.com>
2024-07-01 11:39:40 +0530

    Include persistent options
    Signed-off-by: rohitthakur2590 <rohitthakur2590@outlook.com>

--- plugins/connection/persistent.py
+++ plugins/connection/persistent.py
@@ -72,18 +72,11 @@ class Connection(ConnectionBase):
             "starting connection from persistent connection plugin",
             host=self._play_context.remote_addr,
         )
-        variables = {
-            "ansible_command_timeout": self.get_option(
-                "persistent_command_timeout"
-            ),
-            "remote_addr": self.get_option("remote_addr"),
-            "remote_user": self.get_option("remote_user"),
-            "password": self.get_option("password"),
-            "port": self.get_option("port"),
-            "private_key_file": self.get_option("private_key_file")
-        }
-        self._play_context.connection
-        socket_path = start_connection(self._play_context, variables, self._task_uuid)
+        options = self.get_options()
+        options["ansible_command_timeout"] = self.get_option("persistent_command_timeout")
+        for option in self._options.keys():
+            options[option] = self.get_option(option)
+        socket_path = start_connection(self._play_context, options, self._task_uuid)
         display.vvvv(
             "local domain socket path is %s" % socket_path,
             host=self._play_context.remote_addr,
