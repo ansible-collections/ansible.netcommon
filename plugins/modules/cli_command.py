2faad04c152067d4cdc781402fa12f99a910d58e
roverflow <roverflow5@gmail.com>
2024-07-18 13:44:30 +0530

    fixed docs

--- plugins/modules/cli_command.py
+++ plugins/modules/cli_command.py
@@ -108,17 +108,20 @@ EXAMPLES = """
       - Password
       - Do you want to change that to the standby image
     answer:
+      - y
+      - <password>
+      - y
 
-- name: Simple regexp match for multiple prompt, multiple answer(mandatory check for all prompts)
-  ansible.netcommon.cli_command:
-    command: reload in 5
-    check_all: true
-    prompt:
-    - Save\\?
-    - confirm
-    answer:
-    - n
-    - y
+  - name: Simple regexp match for multiple prompt, multiple answer(mandatory check for all prompts)
+    ansible.netcommon.cli_command:
+      command: reload in 5
+      check_all: true
+      prompt:
+      - Save\\?
+      - confirm
+      answer:
+      - n
+      - y
 """
 
 RETURN = """
