ad71db1d5ba663778e41aa494bd334ec3270f184
Ruchi Pakhle <ruchipakhle@gmail.com>
2025-07-31 23:45:58 +0530

    change

--- plugins/action/network.py
+++ plugins/action/network.py
@@ -19,8 +19,6 @@ from ansible.module_utils.six.moves.urllib.parse import urlsplit
 from ansible.plugins.action.normal import ActionModule as _ActionModule
 from ansible.utils.display import Display
 from ansible.utils.hashing import checksum, checksum_s
-from ansible.vars.clean import remove_internal_keys
-
 
 display = Display()
 
